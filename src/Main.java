93a52f569e7d64e96f0ac64847b96fff366456ea
Dev-Everly <everlysantos98@gmail.com>
2025-04-29 13:18:33 -0300

    feat:ajustando main com while

--- src/Main.java
+++ src/Main.java
@@ -9,18 +9,33 @@ public class Main {
     public static void main(String[] args) throws URISyntaxException {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        ConversorController conversorController = new ConversorController();
+        String resposta = " ";
+        do {
 
-        System.out.println("Qual moeda deseja converter (ex: BRL,USD: )");
-        String origem = scanner.nextLine().toUpperCase();
 
-        System.out.println("Para qual moeda realizar conversão (ex: BRL)");
-        String destino = scanner.nextLine().toUpperCase();
+            ConversorController conversorController = new ConversorController();
 
-        System.out.println("Qual valor deseja converte (ex:20.55)");
-        Double valor = scanner.nextDouble();
+            System.out.println("Qual moeda deseja converter (ex: BRL,USD: )");
+            String origem = scanner.nextLine().toUpperCase();
 
-        conversorController.realizarConversao(origem,destino,valor);
+            System.out.println("Para qual moeda realizar conversão (ex: BRL)");
+            String destino = scanner.nextLine().toUpperCase();
+
+            System.out.println("Qual valor deseja converte (ex:20.55)");
+            Double valor = scanner.nextDouble();
+            scanner.nextLine();
+
+            conversorController.realizarConversao(origem, destino, valor);
+
+            System.out.println("Deseja converter mais moedas? s/n");
+            resposta = scanner.nextLine();
+
+        }while (resposta.equalsIgnoreCase("s"));
+        scanner.close();
+
+        System.out.println("Programa Finalizado");
+
 
     }
 }
+
